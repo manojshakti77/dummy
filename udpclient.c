0975ab366e9f6cdc217f16d1f03497437d766279
Manoj Kumar <manojshakti619@gmail.com>
2017-03-14 18:37:21 +0530

    Update client.c

--- udpclient.c
+++ udpclient.c
@@ -4,6 +4,8 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
+#include<pthread.h> //for threading , link with lpthread
+#include<signal.h>
 
 int flag,alarm_flag,ack;
 
@@ -13,10 +15,10 @@ struct st
 	char buf[100];
 }
 
-thread()
+void *connection_handler(void * client_socket)
 {
     /*Receive message from server*/
-                nBytes = recvfrom(clientSocket,buffer,1024,0,NULL, NULL);
+    nBytes = recvfrom(*((int *)clientSocket),buffer,1024,0,NULL, NULL);
 
     printf("Received from server: %s\n",buffer);
 	if(ack == buffer[0])
@@ -28,7 +30,7 @@ thread()
 		flag = 1;
 	}
 }
-alarm()
+void sig_alarm(int n)
 {
 	if(flag == 1)
 		return ;
@@ -47,6 +49,7 @@ int main(){
   struct st st1;
   struct sockaddr_in serverAddr;
   socklen_t addr_size;
+  pthread_t sniffer_thread;
 
   /*Create UDP socket*/
   clientSocket = socket(PF_INET, SOCK_DGRAM, 0);
@@ -57,11 +60,17 @@ int main(){
   serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
   memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
 
+  signal();
+  
   /*Initialize size variable to be used later on*/
   addr_size = sizeof serverAddr;
   flag = 1;
-  signal();
-  pthread_create();//Thread for reading
+  signal(SIG_ALRM,&sig_alrm);
+  if( pthread_create( &sniffer_thread , NULL ,  connection_handler , (void*) clientSocket) < 0)
+	{
+		perror("could not create thread");
+		return 1;
+	}
 
   while(1){
 	if(flag == 1)
@@ -74,11 +83,14 @@ int main(){
     sendto(clientSocket,buffer,nBytes,0,(struct sockaddr *)&serverAddr,addr_size);
 	alarm(2);
 	while(flag == 0 || alarm_flag == 0);
+	if(flag == 1)
+		alarm(0);
 	}
 	if(alarm_flag == 1)
 	{
 		alarm_flag = 0;
 		sendto(buffer);
+		alarm(2);
 		while(flag == 0 || alarm_flag == 0);
 	}
   }
