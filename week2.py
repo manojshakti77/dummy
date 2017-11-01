3b2ab273df9daf0e0e108c791a33adfc5501c746
= <=>
2017-11-01 03:06:22 -0700

    Adding the boot.txt file to the REPO which has info about the kenel loading logsq

--- week2.py
+++ week2.py
@@ -6,10 +6,11 @@ x_train = []
 y_train = []
 
 for line in f:
-	print("Hello")
 	line = np.array(line.split(),dtype=np.float32)
 	x_train.append(line[:-1])
 	y_train.append(line[:-1])
 
 x_train = np.asmatrix(x_train)
+#print(x_train)
 y_train = np.reshape(y_train,(len(y_train),1))
+print(y_train)
