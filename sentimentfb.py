50d73afcd87ebecf3b2d4dcd6663aa02ec4cb329
manoshakti77 <manojshakti619@gmail.com>
2017-12-21 20:41:33 +0530

    Adding the sentimentfb.py where the basic implementation to access the FB account is performed

---
+++ sentimentfb.py
@@ -0,0 +1,27 @@
+import facebook as fb
+import numpy as np
+
+def connect(access_token,user):
+	print("Connect Entered\n")
+	graph = fb.GraphAPI(access_token)
+	print("Graph Not Null\n")
+	if(graph == 0):
+		print("Graph Null\n")
+	profile = graph.get_object("me")
+	
+	return graph,profile
+	
+	
+def main():
+	print("Sentiment Analysis For Facebook\n");
+	access_token = "EAAZAf6VxNkooBANMeM1W9oD2WIZB02A0Ysl1dN6iZBZCjVHKs0xSpmx9KyHeaNQiV2qTB5zfVtNYavitbwHHYJCBfSWMQYTy9LCN8IZCBZBgq4adEC5aFqwoYPavgQ3eNrTH8RZAZAp77KejtdQtBSbJhofiZC0cqtt3kuKvajWAQaCEeWFIYTi7lAEjXUQfYZAjUZD"
+	user = "manojshakti"
+	
+	graph,profile = connect(access_token, user)
+	
+	friends = graph.get_connections(profile['id'],"comments",limit=1000)
+	#friend_list = [friend['name'] for friend in friends['data']]
+
+	print(friends)
+	
+main()
