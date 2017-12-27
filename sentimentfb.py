6cc6469d841a31d2fa83ea41a126e142a35c1d4b
manoshakti77 <manojshakti619@gmail.com>
2017-12-27 20:19:33 +0530

    Adding the working functionlity Search

--- sentimentfb.py
+++ sentimentfb.py
@@ -1,7 +1,7 @@
-/***************************************************************************************************
-How to use the Facebook developer GraphAPI for retreiving & adding the data to the Facebook Account
-URL: http://facebook-sdk.readthedocs.io/en/latest/api.html
-***************************************************************************************************/
+#/***************************************************************************************************
+#How to use the Facebook developer GraphAPI for retreiving & adding the data to the Facebook Account
+#URL: http://facebook-sdk.readthedocs.io/en/latest/api.html
+#***************************************************************************************************/
 import facebook as fb
 import numpy as np
 
@@ -18,14 +18,19 @@ def connect(access_token,user):
 	
 def main():
 	print("Sentiment Analysis For Facebook\n");
-	access_token = "EAAZAf6VxNkooBANMeM1W9oD2WIZB02A0Ysl1dN6iZBZCjVHKs0xSpmx9KyHeaNQiV2qTB5zfVtNYavitbwHHYJCBfSWMQYTy9LCN8IZCBZBgq4adEC5aFqwoYPavgQ3eNrTH8RZAZAp77KejtdQtBSbJhofiZC0cqtt3kuKvajWAQaCEeWFIYTi7lAEjXUQfYZAjUZD"
+	access_token = "EAAZAf6VxNkooBAO3w4PSm6eEfF0lHRmo57YsqjkuxJ0QijIOXoukYDES9WbcM8U1YQBVdr9EtpMHZA0vtyz3clroMbZC3MZCZAsMbQNksHW1rJkUGRf32GaW8m2unhIVeHOS0spZAkOZAWqynUgUmZBsSkrelRiy2tFPK3LlXl8536a2aKNfZAXviFiu1svUesAQZD"
 	user = "manojshakti"
 	
 	graph,profile = connect(access_token, user)
 	
-	friends = graph.get_connections(profile['id'],"comments",limit=1000)
-	#friend_list = [friend['name'] for friend in friends['data']]
+	users = graph.search(type = "event",q="Christmas",limit=5000)
+	#friends = graph.get_all_connections(profile['id'],"friends",limit=1000)
+	#friend_list = [friend['data'] for friend in friends['data']]
+	
+	for user in users['data']:
+		print('%s %s' % (user['id'],user['name'].encode()))
 
-	print(friends)
+	#print(friends)
+	#print(friend_list)
 	
 main()
